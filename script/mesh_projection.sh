715f055705f4f44399ec0655ece3b0bdd84cc576
DrinkingCoder <drinkingcoder@gmail.com>
2020-05-07 15:51:57 +0800

    configuration

---
+++ script/mesh_projection.sh
@@ -0,0 +1,5 @@
+../bin/render ../src/render 8.1.g_buffer \
+~/Dataset/CambridgeLandmarks/KingsCollege/kingscollege.obj \
+~/Dataset/CambridgeLandmarks/KingsCollege/kingscollege_0311 \
+../bin/seq1/ \
+cambridge_intrinsics.txt
