363dc2fad9b986bbf8c1051a648d4552d9282462
DrinkingCoder <drinkingcoder@gmail.com>
2020-05-07 16:02:35 +0800

    scene coordinate projection

--- script/mesh_projection.sh
+++ script/mesh_projection.sh
@@ -1,4 +1,4 @@
-../bin/render ../src/render 8.1.g_buffer \
+../bin/render ../src/render 8.1.g_buffer.scene_coordinate \
 ~/Dataset/CambridgeLandmarks/KingsCollege/kingscollege.obj \
 ~/Dataset/CambridgeLandmarks/KingsCollege/kingscollege_0311 \
 ../bin/seq1/ \
